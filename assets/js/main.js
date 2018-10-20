8de8dfc70fc135d3e76de6d10e329646bd2a995d
Neverlink <neverlink1337@gmail.com>
2018-10-20 17:46:01 +0300

    Force audio autoplay

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,5 @@
-function clickIE4(){
-	if (event.button==2){
-	return false;
-	}
-	}
-	function clickNS4(e){
-	if (document.layers||document.getElementById&&!document.all){
-	if (e.which==2||e.which==3){
-	return false;
-	}
-	}
-	}
-	if (document.layers){
-	document.captureEvents(Event.MOUSEDOWN);
-	document.onmousedown=clickNS4;
-	}
-	else if (document.all&&!document.getElementById){
-	document.onmousedown=clickIE4;
-	}
-	document.oncontextmenu=new Function("return false")
-
-	video.addEventListener("canplaythrough", function() {
-		// Ready to play whole video?
-	  }, false);
+window.onload = () => {
+    let audio = document.getElementById("bgAudio");
+    audio.volume = 0.07;
+    document.oncontextmenu = () => false;
+}
