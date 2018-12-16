5f876621eb2f8c123bf73f606496a17b64e501d4
Neverlink <neverlink1337@gmail.com>
2018-12-16 22:53:38 +0200

    Change background video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-121281244-1');
+
 window.onload = () => {
     let audio = document.getElementById("bgAudio");
     audio.volume = 0.07;
+
+    var vid = document.getElementById("bgVideo");
+    var aud = document.getElementById("bgAudio");
+
+    aud.currentTime = vid.currentTime;
+    aud.play();
+
     document.oncontextmenu = () => false;
 }
