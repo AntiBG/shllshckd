1ba5427e77c6bc7768f821dac17f68fff567acbc
shllshckd <52105864+shllshckd@users.noreply.github.com>
2019-06-22 23:45:37 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'EU-123456789-9');
+gtag('config', 'UA-121281244-1');
 
 window.onload = () => {
     let audio = document.getElementById("bgAudio");
